fc59f34af637acf4e9baa8c11e675efe6214f24e
harishm11 <harishm11@gmail.com>
2022-09-29 21:17:32 -0700

    9-29-2022

--- myproj/settings.py
+++ myproj/settings.py
@@ -82,13 +82,13 @@ WSGI_APPLICATION = 'myproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME' :'postgres',
-        # 'USER' : 'postgres',
-        # 'PASSWORD': 'readonly',
-        # 'HOST': 'localhost',
-        # 'PORT' :'5432',
+        'ENGINE': 'django.db.backends.postgresql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME' :'djangodb',
+        'USER' : 'postgres',
+        'PASSWORD': 'readonly',
+        'HOST': 'localhost',
+        'PORT' :'5432',
 
     }
 }
