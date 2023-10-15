1962a5bfa308db2af6975de23725c860f6619627
harishm11 <harishm11@gmail.com>
2023-10-15 19:38:49 -0400

    10-15-2023

--- systemtables/models/coverage.py
+++ systemtables/models/coverage.py
@@ -2,6 +2,7 @@ from django.db import models
 from ..models.productcode import ProductCode
 
 
+
 class Coverage(models.Model):
     ProductCd = models.ForeignKey(
         ProductCode, on_delete=models.CASCADE, null=True, blank=True)
@@ -10,13 +11,7 @@ class Coverage(models.Model):
     CoverageType = models.CharField(max_length=50)
     CoverageSequence = models.CharField(max_length=10)
     CoverageLevel = models.CharField(max_length=50)
-    CoverageTerm = models.CharField(max_length=50)
-    OptionValue = models.CharField(max_length=50)
-    OptionDesc = models.CharField(max_length=200)
-    Amount1 = models.CharField(max_length=50, null=True, blank=True)
-    Amount2 = models.CharField(max_length=50, null=True, blank=True)
-    Amount3 = models.CharField(max_length=50, null=True, blank=True)
     EnableInd = models.BooleanField(default=True)
-
     def __str__(self):
-        return f"{self.CoverageName}       {self.OptionValue}"
+        return self.CoverageName
+
